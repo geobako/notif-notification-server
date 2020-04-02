96c300f29a9fdc0be18e6df69164796ca61fcbb7
geobako <giorgosbakogiannis@gmail.com>
2020-04-02 20:29:53 +0300

    d

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const compression = require('compression');
 const router = require('./api/routes/index');
 const morgan = require('morgan');
 const { handleError } = require('./helpers/errorHandling');
+const gcm = require('node-gcm');
 
 const app = express();
 
@@ -26,6 +27,51 @@ app.use(
 );
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cors());
+
+app.get('/notification/:token', (req, res) => {
+    const { token } = req.params;
+    console.log(token);
+    // Create a message
+    // ... with default values
+
+    // ... or some given values
+    const message = new gcm.Message({
+        priority: 'high',
+
+        notification: {
+            title: 'Hello, World',
+            icon: 'ic_launcher',
+            body: 'This is a notification that will be displayed if your app is in the background.'
+        }
+    });
+
+    // Change the message data
+    // ... as key-value
+
+    // ... or as a data object (overwrites previous data object)
+
+    // Set up the sender with you API key
+    const sender = new gcm.Sender(
+        'AAAAVGfXkmU:APA91bEEfALzKRGx22-8Hj7jVtTTpPMrjSeceZ_MigigfRhAoRZoZDjAsk-y1daYqB4bMxn5by0q8CB5WYEnAMCfoHxXmLr--GnxxWB_Y4hYCAo0xTtuXPydLhNWpz_8d_esb6eavm2l'
+    );
+
+    // Add the registration tokens of the devices you want to send to
+    const registrationTokens = [];
+    registrationTokens.push(token);
+
+    // Send the message
+    // ... trying only once
+    sender.send(message, { registrationTokens: registrationTokens }, function(err, response) {
+        if (err) {
+            console.error(err);
+            console.log('error');
+        } else {
+            console.log(response);
+            console.log('success');
+            return res.json({ mes: 'ok' });
+        }
+    });
+});
 
 /*
 Swagger
